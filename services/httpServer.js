fb0d39837c6a96a7024ad38cc464c43bb34295b3
kmnviz <kamenovivanzdravkov@gmail.com>
2024-02-12 09:19:08 +0200

    send messages to event source clients

--- services/httpServer.js
+++ services/httpServer.js
@@ -3,9 +3,19 @@ require('dotenv').config({ path: '../.env' });
 const http = require('http');
 const WebSocket = require('ws');
 
+const eventSourceClients = new Set();
 const httpServer = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, HTTP server is running!');
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    res.write('\n');
+
+    eventSourceClients.add(res);
+
+    req.on('close', () => {
+        eventSourceClients.delete(res);
+    });
 });
 
 const wss = new WebSocket.Server({ server: httpServer });
@@ -14,7 +24,11 @@ wss.on('connection', (ws) => {
     console.log('WebSocket connected');
   
     ws.on('message', (message) => {
-      console.log(`Received message from WebSocket client: `, message);
+        console.log(`Received message from WebSocket client: `, message.toString());
+
+        eventSourceClients.forEach((client) => {
+            client.write(`data: ${message.toString()}\n\n`);
+        });
     });
   
     ws.send('Welcome to the WebSocket server!');
